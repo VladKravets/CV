177f84b6340f6edc3c241069f8b71db19ac05e7e
Vlad Kravets <hazardvlad19@gmail.com>
2022-04-02 11:01:03 +0300

    add component

--- src/Work/Work.js
+++ src/Work/Work.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import styleContainer from "../comman/styles/Container.module.css";
-import {Button} from "../comman/Button/Button";
 import style from './Work.module.css'
 
 
 export const Work = () => {
     return (
         <div className={style.workBlock}>
-            <div className={styleContainer.container}>
+            <div className={`${styleContainer.container} ${style.container}`}>
                 <h2 className={style.title}>Рассматриваю варианты удаленной работы</h2>
-                <Button name={'Нанять меня'}/>
+                <a className={style.button} href="#">Нанять меня</a>
             </div>
         </div>
     );
