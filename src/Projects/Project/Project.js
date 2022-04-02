2692603d37b90fcef8bdbdc53ad039093f92a405
Vlad Kravets <hazardvlad19@gmail.com>
2022-04-02 11:00:45 +0300

    fix styling

--- src/Projects/Project/Project.js
+++ src/Projects/Project/Project.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import style from './Project.module.css'
-import {Button} from "../../comman/Button/Button";
 
 export const Project = () => {
     return (
         <div className={style.project}>
-            Тут будет картинка
-            <Button name={'Смотреть'}/>
-            <div className={style.description}>
-                <h3 className={style.title}>Название проекта</h3>
-                <p className={style.text}>Краткое описание</p>
+            <div className={style.imageProject}>
+                <a className={style.buttonProject} href="#">Посмотреть</a>
             </div>
+            <h3 className={style.title}>Название проекта</h3>
+            <p className={style.description}>Краткое описание</p>
         </div>
     );
 };
