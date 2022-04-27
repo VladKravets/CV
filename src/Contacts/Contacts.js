132f284530d7478d04d0a4d732b8d94105804e13
Vlad Kravets <hazardvlad19@gmail.com>
2022-04-27 14:35:27 +0300

    changed form display

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import style from './Contacts.module.css'
+import styleContainer from '../comman/styles/Container.module.css'
 
 export const Contacts = () => {
     return (
         <div className={style.contactsBlock}>
-            <div className={style.container}>
+            <div className={`${style.container} ${styleContainer.container}`}>
                 <h2 className={style.title}>Contacts</h2>
-                    <form action="#" className={style.contactsForm}>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <textarea></textarea>
-                    </form>
-                    <a href="#"className={style.button}>Отправить</a>
+                <form action="#" className={style.contactsForm}>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <textarea/>
+                <button type={'submit'} className={style.button}>Отправить</button>
+                </form>
             </div>
         </div>
     );
